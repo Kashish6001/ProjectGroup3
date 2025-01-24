29e8cfc547e8cea3e4169923fb838589990d9aff
mohammadkaif <shaikh72@sheridancollege.ca>
2025-01-23 21:19:22 -0500

    Completed task4 : added lucky card

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -4,9 +4,11 @@
  */
 package cardgame;
 
+import java.util.Random;
+
 public class CardGame {
-      
-    private Card[] hand;
+
+    private final Card[] hand;
     private static final int HAND_SIZE = 7;
     private static final Card.Suit[] suits = Card.Suit.values();
 
@@ -16,7 +18,12 @@ public class CardGame {
     }
 
     private void generateHand() {
-        // Complete this for step 3
+        Random random = new Random();
+        for (int i = 0; i < HAND_SIZE; i++) {
+            int value = random.nextInt(13) + 1; // Random value between 1 and 13
+            Card.Suit suit = suits[random.nextInt(suits.length)]; // Random suit
+            hand[i] = new Card(value, suit);
+        }
     }
 
     public void printHand() {
@@ -24,10 +31,22 @@ public class CardGame {
             System.out.println(card.getValue() + " of " + card.getSuit());
         }
     }
-    
+
     public static void main(String[] args) {
         CardGame game = new CardGame();
         game.printHand();
+        
+        Card luckyCard = new Card(Card.Value.SEVEN, Card.Suit.HEARTS); // Hardcoded lucky card
+        System.out.println("\nLucky Card: " + luckyCard.getValue() + " of " + luckyCard.getSuit());
+        if (game.isCardInHand(luckyCard)) {
+            System.out.println("You win! Lucky card is in the magic hand.");
+        } else {
+            System.out.println("You lose! Lucky card is not in the magic hand.");
+        }
+
     }
-    
+
+    private boolean isCardInHand(Card luckyCard) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
